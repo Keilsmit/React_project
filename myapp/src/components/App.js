21501f097d7783da9f08568b0e2eda97853e3617
keilsmit <keilsmit@yahoo.com>
2017-09-13 10:39:26 -0400

    first commit

---
+++ myapp/src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import NavBar from './NavBar';
+import Header from './Header';
+import Section from './Section';
+import Form from './Form';
+import Footer from './Footer';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Header />
+        <Section />
+        <Form />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
